5346653d010b6e4f42a4edc9c1860477f2605417
KevinZonda <KevinZonda@users.noreply.github.com>
2020-09-02 07:24:56 +0800

    trafficstruct: add operator via upload

--- DogYunLib/Model/TrafficStruct.cs
+++ DogYunLib/Model/TrafficStruct.cs
@@ -10,5 +10,25 @@
         public long In { get => InputIn + OutputIn; }
         public long Out { get => InputOut + OutputOut; }
         public double EFR { get => 1.0 * Out / In; }
+
+        public static TrafficStruct operator +(TrafficStruct ts1, TrafficStruct ts2)
+            => new TrafficStruct()
+            {
+                Date = ts1.Date + "+" + ts2.Date,
+                OutputIn = ts1.OutputIn + ts2.OutputIn,
+                OutputOut = ts1.OutputOut + ts2.OutputOut,
+                InputIn = ts1.InputIn + ts2.InputIn,
+                InputOut = ts1.InputOut + ts2.InputOut
+            };
+
+        public static TrafficStruct operator -(TrafficStruct ts1, TrafficStruct ts2)
+            => new TrafficStruct()
+            {
+                Date = ts1.Date + "-" + ts2.Date,
+                OutputIn = ts1.OutputIn - ts2.OutputIn,
+                OutputOut = ts1.OutputOut - ts2.OutputOut,
+                InputIn = ts1.InputIn - ts2.InputIn,
+                InputOut = ts1.InputOut - ts2.InputOut
+            };
     }
 }
